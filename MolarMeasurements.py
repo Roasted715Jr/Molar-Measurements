9f24d96a0ae6132fadc69cbb00656b98e31a49c9
Roasted715Jr <matthew.duphily@live.com>
2017-09-28 13:31:27 -0400

    Added elements to periodic table array

--- MolarMeasurements.py
+++ MolarMeasurements.py
@@ -1,13 +1,27 @@
 #import math
 
-class Element(object):
-    def __init__(self, symbol, atomicWeight):
+class Element (object):
+    def __init__ (self, symbol, atomicNumber, atomicWeight):
         self.symbol = symbol
+        self.atomicNumber = atomicNumber
         self.atomicWeight = atomicWeight
 
-periodicTable = []
-
-userInputVar = NULL
+periodicTable = [
+  	#Just including stuff on data table for lab
+	oxygen = Element("O", 8, 15.9994)
+	silicon = Element("Si", 14, 28.0855)
+  	copper = Element("Cu", 29, 63.456)
+  	sodium = Element("Na", 11, 22.98977)
+  	bromine = Element("Br", 35, 79.904)
+  	iron = Element("Fe", 26, 55.847)
+  	potassium = Element("K", 19, 39.0983)
+  	iodine = Element("I", 53, 126.9045)
+  	manganese = Element("Mn", 25, 54.938)
+  	cobalt = Element("Co", 27, 58.9332)
+  	chlorine = Element("Cl", 17, 35.4527)
+  	hydrogen = Element("H", 1, 1.00797)
+  	calcium = Element("Ca", 20, 40.078)
+]
 
 """for element in periodicTable:
     if userInputVar == element.symbol:
